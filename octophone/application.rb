4f6ee0e1de4fb391738b9f53615dda5749d35008
David Czarnecki <czarneckid@acm.org>
2012-05-23 20:23:34 -0400

    Adding default POST

--- octophone/application.rb
+++ octophone/application.rb
@@ -9,6 +9,28 @@ module Octophone
       "Octophone - Dial into your GitHub repository and merge pull requests from your phone!"
     end
 
+    post '/' do
+      @github = GitHub.new(:oauth_token => ENV['GITHUB_OAUTH_TOKEN'])
+      pull_request = @github.pull_requests.all('czarneckid', 'test-repository').first
+      if pull_request
+        tropo = Tropo::Generator.new do
+          on :event => 'hangup', :next => '/hangup'
+          on :event => 'continue', :next => "/merge_pull_request/1"
+          ask({ :name => 'pull_request_number',
+            :bargein => 'true',
+            :timeout => 30,
+            :require => 'true' }) do
+              say :value => 'Please type in the pull request number to merge'
+              choices :value => '[1 DIGITS]'
+            end
+          end
+
+        tropo.response
+      else
+        Tropo::Generator.say(:value => 'There are no pull requests in your repository. Goodbye.')
+      end
+    end
+
     post '/dialin' do
       @github = GitHub.new(:oauth_token => ENV['GITHUB_OAUTH_TOKEN'])
       pull_request = @github.pull_requests.all('czarneckid', 'test-repository').first
